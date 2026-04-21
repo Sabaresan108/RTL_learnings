3ce5800d73985424407f6e242fa06c1784c86d09
Sabaresan108 <sabaresanpk2004@gmail.com>
2026-04-21 09:54:39 +0530

    Create FSM_multiplier_tb.v

---
+++ FSM_multiplier_tb.v
@@ -0,0 +1,30 @@
+`timescale 1ns/1ps
+module FSM_multiplier_tb;
+reg [7:0]MultiC,MultiP;
+reg clk , rst , start;
+wire [17:0] PR;
+
+FSM_multiplier uut ( clk, rst, start, MultiP, MultiC, PR);
+
+initial
+begin
+	clk = 1; 
+end
+
+always #10 clk = ~clk; 
+
+initial
+begin
+
+rst = 0;
+#10
+rst = 1;
+start = 1;
+MultiC = 8'b0000001;
+MultiP = 8'b10000101;
+#60
+start=0;
+
+$finish;
+end
+endmodule
