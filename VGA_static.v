a09ad8b6810432e5760a5a3ce11ef06173e3a1a7
Sabaresan108 <sabaresanpk2004@gmail.com>
2026-09-08 12:36:07 +0530

    Update VGA_static.v

--- VGA_static.v
+++ VGA_static.v
@@ -13,40 +13,40 @@ always @(posedge clk)
 begin
      if(~rst)
      begin
-          clk_div = 0;
-          h_count = 0;
-          v_count = 0;
-           vga_hs = 1;
-           vga_vs = 1;
-           vga_r  = 5'd00000; 
-           vga_g  = 6'd000000;
-           vga_b  = 5'd00000;
+          clk_div <= 0;
+          h_count <= 0;
+          v_count <= 0;
+           vga_hs <= 1;
+           vga_vs <= 1;
+           vga_r  <= 5'd00000; 
+           vga_g  <= 6'd000000;
+           vga_b  <= 5'd00000;
      end
      
      else
      begin
           if (clk_div == 3'd4) // Clk division (125MHZ)
           begin
-                clk_div = 0;
+                clk_div <= 0;
                 
                 if (h_count == 10'd799) // Horizontal total pixels = 800
                 begin
-                       h_count = 0;
+                       h_count <= 0;
                        
                        if (v_count == 10'd524) // Vertical total lines = 525
                        begin
-                             v_count = 0;
+                             v_count <= 0;
                        end
                         
                        else
                        begin
-                            v_count = v_count + 1;
+                            v_count <= v_count + 1;
                        end 
                 end  
                 
                 else
                 begin
-                     h_count = h_count + 1;
+                     h_count <= h_count + 1;
                 end 
                        
                 if (h_count >= 10'd656 && h_count < 10'd752 ) // Horizontal syncronization [Hsync start(656) + Hsync time(96) = 752 pixels]
@@ -80,44 +80,44 @@ begin
                     
                     else if (h_count < 10'd160) // Yellow
                     begin
-                          vga_r = 5'b11111;
-                          vga_g = 6'b111111;
-                          vga_b = 5'b00000;
+                          vga_r <= 5'b11111;
+                          vga_g <= 6'b111111;
+                          vga_b <= 5'b00000;
                     end
                     
                     else if (h_count < 10'd240) //Cyan
                     begin
-                         vga_r = 5'b00000;
-                         vga_g = 6'b111111;
-                         vga_b = 5'b11111;
+                         vga_r <= 5'b00000;
+                         vga_g <= 6'b111111;
+                         vga_b <= 5'b11111;
                     end
                     
                     else if (h_count < 10'd320) //Green
                     begin
-                         vga_r = 5'b00000;
-                         vga_g = 6'b111111;
-                         vga_b = 5'b00000;
+                         vga_r <= 5'b00000;
+                         vga_g <= 6'b111111;
+                         vga_b <= 5'b00000;
                     end
                     
                     else if (h_count < 10'd400) // Pink
                     begin
-                         vga_r = 5'b11111;
-                         vga_g = 6'b000000;
-                         vga_b = 5'b11111;
+                         vga_r <= 5'b11111;
+                         vga_g <= 6'b000000;
+                         vga_b <= 5'b11111;
                     end
                     
                     else if (h_count < 10'd480) //RED
                     begin
-                         vga_r = 5'b11111;
-                         vga_g = 6'b000000;
-                         vga_b = 5'b00000;
+                         vga_r <= 5'b11111;
+                         vga_g <= 6'b000000;
+                         vga_b <= 5'b00000;
                     end
                                         
                     else if (h_count < 10'd560) //Blue
                     begin
-                         vga_r = 5'b00000;
-                         vga_g = 6'b000000;
-                         vga_b = 5'b11111;
+                         vga_r <= 5'b00000;
+                         vga_g <= 6'b000000;
+                         vga_b <= 5'b11111;
                     end
                     
                     else //Black
